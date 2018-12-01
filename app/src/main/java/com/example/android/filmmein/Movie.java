fc1b55d288f0c1db4bbce485b008b3f3e97be474
bqfix <benjaminfix@gmail.com>
2018-12-01 10:03:46 -0500

    Created Movie Class
    To hold data about each movie

---
+++ app/src/main/java/com/example/android/filmmein/Movie.java
@@ -0,0 +1,46 @@
+package com.example.android.filmmein;
+
+public class Movie {
+    /*********************************************
+     * A class to hold a movie object,           *
+     * parsed from JSON from the theMovieDB API  *
+     *********************************************/
+
+    //Member Variables
+    private String mTitle;
+    private String mReleaseDate;
+    private String mPosterImageLink;
+    private String mVoteAverage;
+    private String mPlotSynopsis;
+
+    //Constructor
+    public Movie(String title, String releaseDate, String posterImageLink, String voteAverage, String plotSynopsis) {
+        mTitle = title;
+        mReleaseDate = releaseDate; //TODO Use Datetime to reformat
+        mPosterImageLink = posterImageLink;
+        mVoteAverage = voteAverage;
+        mPlotSynopsis = plotSynopsis;
+    }
+
+    //Getter methods
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public String getmPosterImageLink() {
+        return mPosterImageLink;
+    }
+
+    public String getmVoteAverage() {
+        return mVoteAverage;
+    }
+
+    public String getmPlotSynopsis() {
+        return mPlotSynopsis;
+    }
+}

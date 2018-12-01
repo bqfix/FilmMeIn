1d145c0a6ed3eb7da52fd4c04af8b3c77b387c97
bqfix <benjaminfix@gmail.com>
2018-12-01 10:43:11 -0500

    Began Recycler Implementation
    Added dependency, added RecyclerView to layout, added RecyclerView class, and left some TODOs for later.

--- app/src/main/java/com/example/android/filmmein/MainActivity.java
+++ app/src/main/java/com/example/android/filmmein/MainActivity.java
@@ -13,10 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO Set RecyclerView (grid layout)
     }
 
     //Menu creation logic
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = this.getMenuInflater();
---
+++ app/src/main/java/com/example/android/filmmein/MovieRecycler.java
@@ -0,0 +1,13 @@
+package com.example.android.filmmein;
+
+import android.support.v7.widget.RecyclerView;
+
+public class MovieRecycler extends RecyclerView {
+
+    //TODO Finish RecyclerView logic
+
+
+
+    //Inner ViewHolder Class
+    //TODO Add ViewHolder Class
+}

93fdc40e58c9fe4fcc0c0999848a3fe0414a3938
bqfix <benjaminfix@gmail.com>
2018-12-01 10:20:44 -0500

    Added attributions
    Get the legal stuff out of the way immediately before I forget.

--- app/src/main/java/com/example/android/filmmein/MainActivity.java
+++ app/src/main/java/com/example/android/filmmein/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.filmmein;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Menu creation logic
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.action_about) :
+                Intent aboutIntent = new Intent(MainActivity.this,AboutActivity.class);
+                startActivity(aboutIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

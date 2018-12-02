8863df5aa3817e88ee7a56a09b148e9656988c9a
bqfix <benjaminfix@gmail.com>
2018-12-02 18:09:50 -0500

    Changed names in MovieAdapter
    Typed it wrong yesterday.

---
+++ app/src/main/java/com/example/android/filmmein/MovieAdapter.java
@@ -0,0 +1,17 @@
+package com.example.android.filmmein;
+
+import android.support.v7.widget.RecyclerView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    //TODO Finish Adapter logic
+
+
+
+    //Inner ViewHolder Class
+    //TODO Add ViewHolder Class
+
+    private class MovieViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}

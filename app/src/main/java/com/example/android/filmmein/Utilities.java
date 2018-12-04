295b27b4febe4d9d7601e0c3824d4df92768d3b2
bqfix <benjaminfix@gmail.com>
2018-12-04 15:41:54 -0500

    Implemented JSON parsing
    Also replaced unnecessary test variables

--- app/src/main/java/com/example/android/filmmein/Utilities.java
+++ app/src/main/java/com/example/android/filmmein/Utilities.java
@@ -3,6 +3,14 @@ package com.example.android.filmmein;
 import android.content.Context;
 import android.content.res.Resources;
 import android.util.DisplayMetrics;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utilities {
     /*********************************************
@@ -22,5 +30,40 @@ public class Utilities {
         return (int) (screenWidth/posterWidth);
     }
 
-    //TODO Add methods for building url, making http request, and parsing JSON
+
+    /** A method to return a List of Movies from the given JSON (likely provided from an http request made to TMdB)
+     *
+     * @param context from the Activity calling the method, used to access resources
+     * @param stringJSON to be parsed
+     * @return the list of movies created from the JSON
+     */
+    public static List<Movie> parseJSON(Context context, String stringJSON){
+        List<Movie> movies = new ArrayList<>();
+        try {
+            JSONObject json = new JSONObject(stringJSON);
+
+            JSONArray moviesArray = json.getJSONArray(context.getString(R.string.json_results_key));
+
+            for (int index = 0; index < moviesArray.length(); index++) {
+                JSONObject currentMovie = moviesArray.getJSONObject(index);
+
+                String title = currentMovie.getString(context.getString(R.string.title_key));
+                String releaseDate = currentMovie.getString(context.getString(R.string.release_date_key));
+                String posterImageLink = context.getString(R.string.poster_image_link_base) + currentMovie.getString(context.getString(R.string.poster_image_link_key)); //JSON only returns end of URL, needs a base, hence concatenation
+                double voterAverage = currentMovie.getDouble(context.getString(R.string.voter_average_key));
+                String plotSynopsis = currentMovie.getString(context.getString(R.string.plot_synopsis_key));
+
+                movies.add(new Movie(title,releaseDate,posterImageLink,voterAverage,plotSynopsis));
+            }
+
+        } catch (JSONException exception) {
+            Log.e(context.getClass().getSimpleName(), exception.getMessage());
+        }
+
+
+        return movies;
+    }
+
+
+    //TODO Add methods for building url, and making http request
 }
